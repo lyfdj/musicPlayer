f5e1786c6552160b8f37d0ae928e7fa46938c9b4
李玉芳 <1971521448@qq.com>
2019-11-27 16:29:25 +0800

    提交基础代码

---
+++ music/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 19715 on 2019/11/26.
+ */
+$(function () {
+    //引入滚动条插件
+    $(".main-left").mCustomScrollbar();
+//    给鼠标添加移入移出事件
+    $(".title").mouseenter(function () {
+        // $(this).stop().addClass("mouse-general");
+        // $(this).sibling().stop().removeClass("mouse-general");
+        // alert("jsfk")
+    });
+//     $(".title").delegate("")
+});
